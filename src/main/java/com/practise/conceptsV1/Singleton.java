7205de253321a54ed17495577868a49bb1bd0cc6
keerthana <keerthanachowdary.challa@abzooba.com>
2017-11-09 13:55:22 +0530

    singletonStudent instance

--- src/main/java/com/practise/conceptsV1/Singleton.java
+++ src/main/java/com/practise/conceptsV1/Singleton.java
@@ -2,9 +2,23 @@ package com.practise.conceptsV1;
 
 public class Singleton {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//just some random variables
 
+	public String name;
+	public int rollNo;
+	public boolean admit;
+
+	// private constructor 
+
+	private Singleton() {
+		admit = true;
+		name = "keerthana";
+		rollNo = 1;
+	}
+
+	public static void main(String[] args) {
+		Singleton justOneObject = new Singleton();
+		System.out.println("name: " + justOneObject.name + "\nrollNo: " + justOneObject.rollNo + "\nadmit: " + justOneObject.admit);
 	}
 
 }
